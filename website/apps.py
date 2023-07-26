f7a3ac575fa827c9dc9ba5bc60c409a8098b2c9a
Abdur Rehman <rehmanabdur824@gmail.com>
2023-07-26 16:22:41 +0500

    Registration & Login updated

--- website/apps.py
+++ website/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CrmWebsiteConfig(AppConfig):
+class WebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CRM_website'
+    name = 'website'
